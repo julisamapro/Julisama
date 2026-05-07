3f3fe03c262e96bac719e9b6da2b9d4df59541a4
Nebji14 <bendav192837@gmail.com>
2026-05-07 09:56:11 +0200

    fix indexation

--- next.config.mjs
+++ next.config.mjs
@@ -15,19 +15,33 @@ const nextConfig = {
   },
   async redirects() {
     return [
+      // ANCIENNES PAGES STANDARDS
+      { source: '/a-propos', destination: '/#about', permanent: true },
+      { source: '/a-propos/', destination: '/#about', permanent: true },
+      { source: '/cgv/', destination: '/cgv', permanent: true },
+
+      // ANCIENS DÉTAILS DE PRESTATIONS WORDPRESS
       {
-        source: '/a-propos',
-        destination: '/#about',
+        source: '/details-investissement-meuble/:path*',
+        destination: '/#services',
         permanent: true,
       },
       {
-        source: '/a-propos/',
-        destination: '/#about',
+        source: '/details-rdv-coaching/:path*',
+        destination: '/#services',
         permanent: true,
       },
       {
-        source: '/cgv/',
-        destination: '/cgv',
+        source: '/details-immersion-deco/:path*',
+        destination: '/#services',
+        permanent: true,
+      },
+
+      // NETTOYAGE DES RECHERCHES WORDPRESS
+      {
+        source: '/:path*',
+        has: [{ type: 'query', key: 's' }],
+        destination: '/',
         permanent: true,
       },
     ];
